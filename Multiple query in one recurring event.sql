405050976b126dcce384d4ac015949c3afb79876
Ashik Sazal <53079792+AshikSazal@users.noreply.github.com>
2020-11-20 18:03:24 +0600

    Create Multiple query in one recurring event.sql

---
+++ Multiple query in one recurring event.sql
@@ -0,0 +1,14 @@
+CREATE TABLE review3(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,product_id INT,
+mean_rating DOUBLE)
+
+DELIMITER |
+CREATE event event1 ON SCHEDULE EVERY 20 SECOND DO
+BEGIN
+INSERT INTO review3(product_id,mean_rating)SELECT review.product_id,SUM(review.rating)/COUNT(review.product_id) 
+FROM review,review2 WHERE review.product_id=review2.id GROUP BY review.product_id;
+
+UPDATE review2,review3 SET review2.rating=review3.mean_rating WHERE review2.id=review3.product_id;
+
+TRUNCATE review3;
+END |
+DELIMITER;
